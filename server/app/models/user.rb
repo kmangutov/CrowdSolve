6842638db1a08b1c5c26f86550dd30758f709b91
Artyom Pavlichenko <brentwoodpav@gmail.com>
2015-03-21 09:31:13 -0500

    users working

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
+	# Set uniqueness of the user
+	validates_uniqueness_of :email
+
+	# We need a password hash
 	has_secure_password
+
 end
---
+++ server/app/src/main/java/com/kmangutov/crowdsolve/models/Question.java
@@ -0,0 +1,12 @@
+package com.kmangutov.crowdsolve.models;
+
+import java.util.List;
+
+/**
+ * Created by kmangutov on 3/21/15.
+ */
+public class Question {
+
+    public String text;
+    public List<String> options;
+}
